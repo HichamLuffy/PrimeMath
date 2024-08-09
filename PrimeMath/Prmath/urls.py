3dcb0202c44748668f9481e912f2d7cb658af94e
HichamLuffy <imalexvssteve2020@gmail.com>
2024-08-09 16:53:57 +0100

    setup pages

---
+++ PrimeMath/Prmath/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.main, name='main'),  # Main page route
+    path('Login', views.SignUp, name='Login'),
+    path('Register', views.Register, name='Register'),
+    path('Courses', views.Courses, name='Courses'),
+    path('Dashboard', views.Dashboard, name='Dashboard'),
+    path('Profile', views.Profile, name='Profile'),
+    path('Users', views.Users, name='Users'),
+]
--- PrimeMath/Prmath/views.py
+++ PrimeMath/Prmath/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
+
 
 # Create your views here.
+def main(request):
+    return HttpResponse("PrimeMath")
+    #return render(request, 'main.html')
+
+
+def Login(request):
+    return HttpResponse("SignUp")
+    #return render(request, 'main.html')
+
+def Register(request):
+    return HttpResponse("Register")
+    #return render(request, 'main.html')
+
+def Courses(request):
+    return HttpResponse("courses")
+    #return render(request, 'main.html')
+
+def Dashboard(request):
+    return HttpResponse("Dashboard")
+    #return render(request, 'main.html')
+
+def Profile(request):
+    return HttpResponse("Profile")
+    #return render(request, 'main.html')
+
+def Users(request):
+    return HttpResponse("Users")
+    #return render(request, 'main.html')
+
+def About(request):
+    return HttpResponse("About")
+    #return render(request, 'main.html')
