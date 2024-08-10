4b89827121d80be18e0b1ada82ca4f7f6d371096
HichamLuffy <imalexvssteve2020@gmail.com>
2024-08-10 16:13:47 +0100

    aaaaaa

---
+++ PrimeMath/Prmath/decorators.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponseForbidden
+
+def student_required(view_func):
+    def wrapper_func(request, *args, **kwargs):
+        if hasattr(request.user, 'profile') and request.user.profile.role == 'student':
+            return view_func(request, *args, **kwargs)
+        else:
+            return HttpResponseForbidden("You must be a student to access this page.")
+    return wrapper_func
+
+def student_required(view_func):
+    def wrapper_func(request, *args, **kwargs):
+        if hasattr(request.user, 'profile') and request.user.profile.role == 'teacher':
+            return view_func(request, *args, **kwargs)
+        else:
+            return HttpResponseForbidden("You must be a teacher to access this page.")
+    return wrapper_func
--- PrimeMath/Prmath/views.py
+++ PrimeMath/Prmath/views.py
@@ -1,20 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+from django.contrib.auth.models import User
 from django.shortcuts import HttpResponse
-from .models import User, Courses, Projects
-
+from django.contrib import messages
+from .models import User, Courses, Projects, Profile
+from .decorators import student_required, teacher_required
 
 # Create your views here.
-def main(request, id):
+
+@student_required
+def student_dashboard(request, id):
     course1 = Courses.objects.get(id=id)
     return HttpResponse("PrimeMath %s" %course1.name)
     #return render(request, 'main.html')
 
 
+
+
 def Login(request):
+
     return HttpResponse("SignUp")
     #return render(request, 'main.html')
 
 def Register(request):
+    if request.method == 'POST':
+        username = request.Post['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        role = request.POST['role']
+        if password1 == password2:
+            if User.objects.filter(username=username).exists() or User.objects.filter(email=email).exists():
+                messages.error(request, 'Username or email already exists')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password1)
+                user.save()
+
+                user_model = User.objects.get(username=username)
+                if role == 'student':
+                    age = request.POST['age']
+                    current_study = request['current_study']
+                    new_profile = Profile.objects.create(user=user_model, role='student', age=age, current_study=current_study)
+                elif role == 'teacher':
+                    status = request.POST['status'] # Real teacher, degree holder, etc.
+                    teaching_experience = request.POST['teaching_experience']
+                    subjects_of_expertise = request.POST.getlist('subjects_of_expertise')
+                    certifications = request.POST['certifications']
+                    new_profile = Profile.objects.create(user=user_model, role='teacher', status = status, teaching_experience = teaching_experience, subjects_of_expertise = subjects_of_expertise, certifications = certifications)
+                new_profile.save()
+                messages.success(request, 'Registration successful')
+                return redirect('login')
     return HttpResponse("Register")
     #return render(request, 'main.html')
 
